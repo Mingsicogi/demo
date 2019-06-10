124b22f6b50c6680c6fad9b7813a076495b1b1d1
Mingsicogi <codedoctor119@gmail.com>
2019-06-11 03:40:25 +0900

    +line login

--- src/main/java/com/example/demo/app/common/controller/Main.java
+++ src/main/java/com/example/demo/app/common/controller/Main.java
@@ -2,11 +2,15 @@ package com.example.demo.app.common.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class Main {
+
+    private String chanelId = "1584207381";
+    private String callBackUrl = "http://1.240.34.111:18080/main";
 
     @RequestMapping(value = {"", "/", "/main"})
     public String main(@RequestParam(name = "name", required = false, defaultValue = "Guest") String name, Model model) {
@@ -14,5 +18,14 @@ public class Main {
         model.addAttribute("name", name);
 
         return "main";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        String oAuthUrl = "https://access.line.me/dialog/oauth/weblogin?response_type=code&client_id=%s&redirect_uri=%s&state=123";
+
+        oAuthUrl = String.format(oAuthUrl, chanelId, callBackUrl);
+
+        return "redirect:" + oAuthUrl;
     }
 }
---
+++ src/main/java/com/example/demo/app/common/service/CommonService.java
@@ -0,0 +1,7 @@
+package com.example.demo.app.common.service;
+
+
+public interface CommonService {
+
+
+}
